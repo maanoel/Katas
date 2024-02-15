ac010521a814fbc269a817a042b9f0fdd1475d4d
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2024-02-15 00:52:48 -0300

    feat: Coding kata.

---
+++ Kata.cs
@@ -0,0 +1,16 @@
+using System.Linq;
+
+
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+String will never be empty and you do not need to account for different data types.
+
+*/
+
+public class Kata
+{
+  public static int FindShort(string s)
+  {
+    return s.Split(" ").Min(word => word.Length);
+  }
+}
