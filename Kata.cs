28d910a3848a31091827cef4c016437651f70d9e
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2024-02-15 21:33:35 -0300

    feat: Sec

--- Kata.cs
+++ Kata.cs
@@ -1,14 +1,36 @@
 using System.Linq;
 
 /*
-Simple, given a string of words, return the length of the shortest word(s).
-String will never be empty and you do not need to account for different data types.
+
+Given an array (a list in Python) of integers and an integer n, find all occurrences of n in the given array and return another array containing all the index positions of n in the given array.
+
+If n is not in the given array, return an empty array [].
+
+Assume that n and all values in the given array will always be integers.
+
 */
 
-public class Kata
+public class Kata2
 {
-  public static int FindShort(string s)
+  public static int[] FindAll(int[] array, int n)
   {
-    return s.Split(" ").Min(word => word.Length);
+    return Enumerable.Range(0, array.Length).Where(i=> array[i] == n).ToArray();
+  }
+  
+  public static int[] FindAllTwo(int[] array, int n)
+  {
+    int index = -1;
+    var indexes = new List<int>();
+    
+    do{
+      index = Array.IndexOf(array, n, index + 1);
+      
+      if(index!= -1){
+        indexes.Add(index);
+      }
+    }
+    while(index != -1);
+    
+    return indexes.ToArray();
   }
 }
